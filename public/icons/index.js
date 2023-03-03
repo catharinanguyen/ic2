9b226804629a377ae2e819b05a2f2f9228256946
Do Thien Tu Nguyen <47329148+catharinanguyen@users.noreply.github.com>
2023-03-03 16:45:11 +0700

    update manage page style

---
+++ public/icons/index.js
@@ -0,0 +1,23 @@
+import Layout01 from "./layout_01.svg";
+import Layout02 from "./layout_03.svg";
+import Layout03 from "./layout_02.svg";
+import Layout04 from "./layout_04.svg";
+import Layout05 from "./layout_05.svg";
+import Layout06 from "./layout_06.svg";
+import Layout07 from "./layout_07.svg";
+import Map from "./map.svg";
+import Agenda from "./agenda.svg";
+import Audio from "./audio.svg";
+
+export default {
+  Layout01,
+  Layout02,
+  Layout03,
+  Layout04,
+  Layout05,
+  Layout06,
+  Layout07,
+  Map,
+  Agenda,
+  Audio,
+};
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,11 +8,25 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    colors: {
+      transparent: "transparent",
+      current: "currentColor",
+      white: "#ffffff",
+      blue: "#0056FE",
+      green: "#04D5B7",
+      card: "rgba(26, 36, 51, 0.8)",
+      card1: "rgba(32, 71, 94, 0.8)",
+      black: "#0000",
+    },
+    fontFamily: {
+      sans: ["Montserrat", "sans-serif"],
+      serif: ["Merriweather", "serif"],
+    },
     extend: {
       backgroundImage: {
-        'bottom-bar-bg': "url('/images/bottom-bar-bg.svg')",
-      }
+        "bottom-bar-bg": "url('/images/bottom-bar-bg.svg')",
+      },
     },
   },
   plugins: [],
-}
+};
