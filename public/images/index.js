8dd8be11c99bc82b0b68c0884c420243f3aac680
Harry Nguyen <hai@qamera.com>
2023-03-08 14:56:34 +0700

    feature/add home sidebar

---
+++ public/images/index.js
@@ -0,0 +1,9 @@
+import MapWidget from './map-widget.svg';
+import CalendarWidget from './calendar-widget.svg';
+import MusicWidget from './music-widget.svg';
+
+export default {
+  MapWidget,
+  CalendarWidget,
+  MusicWidget,
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,9 @@
 import { useReducer } from "react";
 
 import Head from "next/head";
+import Image from "next/image";
+
+
 import Button1 from "@/components/Button1";
 import Button2 from "@/components/Button2";
 import Button3 from "@/components/Button3";
@@ -9,6 +12,8 @@ import Button5 from "@/components/Button5";
 import Button7 from "@/components/Button7";
 import Button8 from "@/components/Button8";
 import SettingsButton from "@/components/SettingsButton";
+
+import images from '../../public/images';
 
 export default function Home() {
   const [localState, updateLocalState] = useReducer(
@@ -31,58 +36,61 @@ export default function Home() {
       <div className="bg-black h-full w-full max-w-[1180px] max-h-[820px] m-auto relative">
         <div className="flex flex-1">
           <div className="w-fit h-fit max-h-[820px]">
-            <img
+            <Image
               src="/images/left-pane.svg"
               alt="left-pane"
-              className=" object-contain w-fit h-fit max-h-[820px]"
+              width={320}
+              height={820}
+              className=" object-contain w-[320px] h-fit max-h-[820px]"
             />
           </div>
           <div className="mx-auto box-border flex-col h-full max-h-[820px] items-center justify-center">
-            <div className="flex grow h-[800px]">
-              <div className="grid grid-cols-2 grid-rows-2 gap-3 p-2 mx-auto box-border h-full">
-                <div className="row-span-2 h-full max-h-[820px]">
-                  <img
-                    className="object-contain h-full"
-                    src="/images/map-widget.svg"
-                    alt="map-widget"
-                  />
-                </div>
-                <div className="w-full h-full">
-                  <img
-                    className="object-contain h-full"
-                    src="/images/calendar-widget.svg"
-                    alt="calendar-widget"
-                  />
-                </div>
-                <div className="w-full h-full">
-                  <img
-                    className="object-contain h-full"
-                    src="/images/music-widget.svg"
-                    alt="music-widget"
-                  />
-                </div>
+            {/* <div className="flex grow h-[800px]"> */}
+            <div className="grid grid-cols-2 grid-rows-2 gap-3 p-2 mx-auto box-border h-full">
+              <div className="row-span-2 max-h-[820px]">
+                <Image
+                  className="object-contain h-full"
+                  src={images.MapWidget}
+                  alt="map-widget"
+                />
               </div>
-            </div>
-            {localState.openBottomBar === false && (
-              <div className="flex flex-none items-center w-full">
-                <button
-                  type="button"
-                  className="h-4 mx-auto"
-                  onClick={() => updateLocalState({ openBottomBar: true })}
-                >
-                  <svg
-                    width="174"
-                    height="4"
-                    viewBox="0 0 180 4"
-                    fill="none"
-                    xmlns="http://www.w3.org/2000/svg"
+              <div className="w-full h-full">
+                <Image
+                  className="object-contain h-full"
+                  src={images.CalendarWidget}
+                  alt="calendar-widget"
+                />
+              </div>
+              <div className="w-full h-full">
+                <Image
+                  className="object-contain h-full"
+                  src={images.MusicWidget}
+                  alt="music-widget"
+                />
+              </div>
+              {localState.openBottomBar === false && (
+                // <div className="flex flex-none items-center w-full">
+                <div className="col-span-2">
+                  <button
+                    type="button"
+                    className="h-4 mx-auto"
+                    onClick={() => updateLocalState({ openBottomBar: true })}
                   >
-                    <rect x="0" width="180" height="4" rx="2" fill="#04D5B7" />
-                  </svg>
-                  <span className="sr-only">Icon description</span>
-                </button>
-              </div>
-            )}
+                    <svg
+                      width="174"
+                      height="4"
+                      viewBox="0 0 180 4"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <rect x="0" width="180" height="4" rx="2" fill="#04D5B7" />
+                    </svg>
+                    <span className="sr-only">Icon description</span>
+                  </button>
+                </div>
+              )}
+            </div>
+            {/* </div> */}
           </div>
         </div>
         {localState.openBottomBar === true && (
