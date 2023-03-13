4c15800124d4a7babe93fe9a6a231a74cb88e56c
Do Thien Tu Nguyen <47329148+catharinanguyen@users.noreply.github.com>
2023-03-13 16:27:38 +0700

    change color of widgets on edit page

--- src/components/layouts/FourCellsLayout.jsx
+++ src/components/layouts/FourCellsLayout.jsx
@@ -1,20 +1,45 @@
+import { THEME_KEYS } from "@/constants/constants";
+import { selectCurrentTheme } from "@/store/slices/appStatusSlice";
 import Image from "next/image";
+import { useSelector } from "react-redux";
 
 import image from "../../../public/icons";
 
 function FourCellsLayout() {
+  const gTheme = useSelector(selectCurrentTheme);
+
   return (
     <div className="grid grid-cols-2 grid-rows-4 gap-3 p-2 mx-auto box-border h-full">
-      <div className="row-span-2 card">
+      <div
+        className={
+          "row-span-2 " +
+          (gTheme == THEME_KEYS.SOPHISTICATED ? "card" : "card-simplicity")
+        }
+      >
         <Image src={image.Agenda} alt="Agenda" />
       </div>
-      <div className="row-span-2 card">
+      <div
+        className={
+          "row-span-2 " +
+          (gTheme == THEME_KEYS.SOPHISTICATED ? "card" : "card-simplicity")
+        }
+      >
         <Image src={image.Audio} alt="Audio" />
       </div>
-      <div className="row-span-2 card">
+      <div
+        className={
+          "row-span-2 " +
+          (gTheme == THEME_KEYS.SOPHISTICATED ? "card" : "card-simplicity")
+        }
+      >
         <Image src={image.Map} alt="Map" />
       </div>
-      <div className="row-span-2 card">
+      <div
+        className={
+          "row-span-2 " +
+          (gTheme == THEME_KEYS.SOPHISTICATED ? "card" : "card-simplicity")
+        }
+      >
         <Image src={image.Map} alt="Map" />
       </div>
     </div>
--- src/components/layouts/TwoRowsLayoutEdit.jsx
+++ src/components/layouts/TwoRowsLayoutEdit.jsx
@@ -1,14 +1,29 @@
+import { THEME_KEYS } from "@/constants/constants";
+import { selectCurrentTheme } from "@/store/slices/appStatusSlice";
 import Image from "next/image";
+import { useSelector } from "react-redux";
 
 import image from "../../../public/icons";
 
 function TwoRowsLayoutEdit() {
+  const gTheme = useSelector(selectCurrentTheme);
+
   return (
     <div className="grid grid-cols-2 grid-rows-4 gap-3 p-2 mx-auto box-border h-full">
-      <div className="col-span-2 row-span-2 card">
+      <div
+        className={
+          "col-span-2 row-span-2 " +
+          (gTheme == THEME_KEYS.SOPHISTICATED ? "card" : "card-simplicity")
+        }
+      >
         <Image src={image.Map} alt="Map" />
       </div>
-      <div className="col-span-2 row-span-2 card">
+      <div
+        className={
+          "col-span-2 row-span-2 " +
+          (gTheme == THEME_KEYS.SOPHISTICATED ? "card" : "card-simplicity")
+        }
+      >
         <Image src={image.Audio} alt="Audio" />
       </div>
     </div>
