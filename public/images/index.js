b61c0485cec3456fa0ee750890cdf524bc5b60d2
Do Thien Tu Nguyen <47329148+catharinanguyen@users.noreply.github.com>
2023-03-31 18:22:04 +0700

    restore file index.js

---
+++ public/images/index.js
@@ -0,0 +1,44 @@
+/* eslint-disable import/no-anonymous-default-export */
+import MapWidget from "./map-widget.svg";
+import LeftPanelDark from "./left-panel-dark.svg";
+import LeftPanelLight from "./left-panel-light.svg";
+import MapWidgetVertical from "./map-widget-vertical.jpg";
+import MapWidgetVerticalLight from "./map-widget-vertical-light.jpg";
+import MapWidgetHorizontal from "./map-widget-horizontal.jpg";
+import MapWidgetHorizontalLight from "./map-widget-horizontal-light.jpg";
+import WeatherWidgetHorizontal from "./weather-widget-horizontal.jpg";
+import WeatherWidgetHorizontalLight from "./weather-widget-horizontal-light.jpg";
+import CalendarWidget from "./calendar-widget.svg";
+import CalendarWidgetLight from "./calendar-widget-light.jpg";
+import MusicWidget from "./music-widget.svg";
+import MusicWidgetLight from "./music-widget-light.jpg";
+import MessagesWidget from "./messages-widget.jpg";
+import MessagesWidgetLight from "./messages-widget-light.jpg";
+import ListWidget from "./list-widget.jpg";
+import ListWidgetLight from "./list-widget-light.jpg";
+import SophisticatedTheme from "./sophi.png";
+import ModernTheme from "./modern.png";
+import RetroTheme from "./retro.png";
+
+export default {
+  MapWidget,
+  MapWidgetVertical,
+  MapWidgetVerticalLight,
+  MapWidgetHorizontal,
+  MapWidgetHorizontalLight,
+  CalendarWidget,
+  CalendarWidgetLight,
+  MusicWidget,
+  MusicWidgetLight,
+  WeatherWidgetHorizontal,
+  WeatherWidgetHorizontalLight,
+  MessagesWidget,
+  MessagesWidgetLight,
+  ListWidget,
+  ListWidgetLight,
+  SophisticatedTheme,
+  ModernTheme,
+  RetroTheme,
+  LeftPanelDark,
+  LeftPanelLight,
+};
