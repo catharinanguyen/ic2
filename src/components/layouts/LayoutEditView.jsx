e58175ca362f7c55e58f830f3d1361d0e91f5cd7
Do Thien Tu Nguyen <47329148+catharinanguyen@users.noreply.github.com>
2023-04-10 16:37:03 +0700

    change background of control page to uploaded image

--- src/components/layouts/LayoutEditView.jsx
+++ src/components/layouts/LayoutEditView.jsx
@@ -2,6 +2,7 @@ import clsx from "clsx";
 import { useSelector } from "react-redux";
 
 import {
+  selectBackgroundImage,
   selectCurrentPage,
   selectCurrentTheme,
   selectPrimaryColor,
@@ -12,16 +13,25 @@ import WidgetIconWithName from "@/components/WidgetIconWithName";
 import WidgetSelectionGrid from "@/components/WidgetSelectionGrid";
 
 import { THEME_KEYS } from "@/constants/constants";
+import hexToRgba from "hex-to-rgba";
 
 function LayoutEditView(props) {
   const gTheme = useSelector(selectCurrentTheme);
   const gPrimaryColor = useSelector(selectPrimaryColor);
   const gCurrentPage = useSelector(selectCurrentPage);
   const gPageInfo = useSelector(selectPageInfo(gCurrentPage));
+  const gBackgroundImage = useSelector(selectBackgroundImage);
 
   const { onSelectWidget, position, selectedWidgetPosition } = props;
 
   const activeBorderClass = { borderWidth: "2px", borderColor: gPrimaryColor };
+
+  const backgroundStyle =
+    gBackgroundImage === undefined || gBackgroundImage === null
+      ? gTheme == THEME_KEYS.SOPHISTICATED
+        ? { background: `linear-gradient( rgba(26, 36, 51, 0.8), rgba(32, 71, 94, 0.8))` }
+        : { backgroundColor: "#ffffff" }
+      : { backgroundColor: hexToRgba(gPrimaryColor, 0.8) };
 
   return (
     <div
@@ -30,7 +40,9 @@ function LayoutEditView(props) {
         gTheme == THEME_KEYS.SOPHISTICATED ? "card" : "card-simplicity"
       )}
       style={
-        selectedWidgetPosition === position ? { ...activeBorderClass } : {}
+        selectedWidgetPosition === position
+          ? { ...activeBorderClass, ...backgroundStyle }
+          : { ...backgroundStyle }
       }
       onClick={() => onSelectWidget(position)}
     >
