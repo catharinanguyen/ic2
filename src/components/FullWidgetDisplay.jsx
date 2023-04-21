84f85a831b64cfcc6bf4ce314568e11eefc8bd8f
Do Thien Tu Nguyen <47329148+catharinanguyen@users.noreply.github.com>
2023-04-21 12:03:02 +0700

    fix full widget display

--- src/components/FullWidgetDisplay.jsx
+++ src/components/FullWidgetDisplay.jsx
@@ -1,9 +1,6 @@
 import { useDispatch, useSelector } from "react-redux";
 
-import {
-  resetFullWidget,
-  selectFullWidget,
-} from "@/store/slices/appStatusSlice";
+import { resetFullWidget, selectFullWidget } from "@/store/slices/appStatusSlice";
 
 import WidgetDisplay from "@/components/WidgetDisplay";
 import CloseButton from "@/components/CloseButton";
@@ -16,11 +13,8 @@ function FullWidgetDisplay() {
   const gFullWidget = useSelector(selectFullWidget);
 
   return (
-    <div className="w-[842px] h-[800px] m-auto mb-2 box-border relative overflow-hidden ">
-      <WidgetDisplay
-        widgetType={gFullWidget}
-        widgetShape={WIDGET_SHAPES.FULL}
-      />
+    <div className="w-[842px] h-[760px] portrait:h-[64.254vw] m-auto mb-2 box-border relative overflow-hidden -top-[16px]">
+      <WidgetDisplay widgetType={gFullWidget} widgetShape={WIDGET_SHAPES.FULL} />
       <div className="absolute left-0 top-0">
         <CloseButton onClick={() => dispatch(resetFullWidget())} />
       </div>
